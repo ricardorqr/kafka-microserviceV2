bc518f47a7e7722872da44d8ebdc41f26251435b
Ricardo Ribeiro <ricardorqr@gmail.com>
2022-03-18 20:38:56 -0400

    Initial version

---
+++ src/main/java/com/kafka/microservice/KafkaMicroserviceV2Application.java
@@ -0,0 +1,13 @@
+package com.kafka.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMicroserviceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMicroserviceV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/microservice/KafkaMicroserviceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMicroserviceV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
